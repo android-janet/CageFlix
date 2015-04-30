8c6c72d4c1e9f46c6ab35a933e73f379d2e4c220
Melissa Khat <melissa.khat@gmail.com>
2015-04-29 22:08:47 -0600

    create new review form, add movie id column to review table

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,19 @@
 class ReviewsController < ApplicationController
    def new
+      @review = Review.new
+   end
+
+   def create
+      @review = Review.new(review_params)
+      if @review.save
+         redirect_to reviews_path
+      else
+         render :new
+      end
+   end
+
+   private
+   def review_params
+      params.require(:review).permit(:title, :body)
    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429233455) do
+ActiveRecord::Schema.define(version: 20150430035007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20150429233455) do
     t.text     "synopsis"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.string "title"
+    t.text   "body"
   end
 
 end
