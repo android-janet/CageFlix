ca0ceb5e38037afde721f85c7348a50bd21b808a
Melissa Khat <melissa.khat@gmail.com>
2015-04-29 21:19:59 -0600

    migrate movies table

--- db/schema.rb
+++ db/schema.rb
@@ -11,31 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426000700) do
+ActiveRecord::Schema.define(version: 20150429233455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "movies", force: true do |t|
     t.string   "name"
-    t.date     "year"
+    t.string   "year"
     t.text     "synopsis"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "reviews", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "movie_id"
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string "username"
-    t.string "password_digest"
   end
 
 end
