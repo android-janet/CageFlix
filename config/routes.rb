84627734e68e210b2c8204501cb148b2fb22c847
Melissa Khat <melissa.khat@gmail.com>
2015-05-02 18:40:35 -0600

    user can visit review page for a movie

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :movies 
+  resources :movies
   resources :reviews
 
+  resources :movies do
+  resources :reviews
+end
 
 end
